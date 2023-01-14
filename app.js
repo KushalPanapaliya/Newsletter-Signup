916129f95158f61471a899873f809a3b085e8e30
KushalPanapaliya <102788484+KushalPanapaliya@users.noreply.github.com>
2023-01-14 18:29:57 +0530

    Changed port from 3000 to process.env.PORT

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.post("/failure", function(req, res){
     res.redirect("/");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, function(){
     console.log("Server is Alive! on port 3000.");
 });
 
